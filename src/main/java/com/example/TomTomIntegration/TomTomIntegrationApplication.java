73e94675b46437022c469a06ba0ee1f5997f7204
112seany <andrey.efimenko1488@gmail.com>
2023-07-10 12:25:35 +0300

    Initial commit

---
+++ src/main/java/com/example/TomTomIntegration/TomTomIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.TomTomIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomTomIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomTomIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TomTomIntegration/TomTomIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TomTomIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomTomIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
