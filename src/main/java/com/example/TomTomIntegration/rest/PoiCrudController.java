af6dc20ee453ba9f57e27a428194bd737e371868
112seany <andrey.efimenko1488@gmail.com>
2023-08-06 23:07:45 +0300

    fixed crudController

--- src/main/java/com/example/TomTomIntegration/rest/PoiCrudController.java
+++ src/main/java/com/example/TomTomIntegration/rest/PoiCrudController.java
@@ -3,6 +3,9 @@ package com.example.TomTomIntegration.rest;
 import com.example.TomTomIntegration.facade.PoiFacade;
 import com.example.TomTomIntegration.rest.request.PoiCreationRequest;
 import com.example.TomTomIntegration.rest.response.PoiCreationResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,10 +16,10 @@ public class PoiCrudController {
 
     private final PoiFacade poiFacade;
 
+    @Operation(summary = "Create POI", description = "Returns created POI")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Successfully created")})
     @PostMapping
     public PoiCreationResponse createPOI(@RequestBody PoiCreationRequest request) {
         return poiFacade.createPOI(request);
     }
-
-
 }
