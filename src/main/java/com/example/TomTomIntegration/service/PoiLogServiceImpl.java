3abf2914d71236be50bd1f53e069a8c06cf1b39c
112seany <andrey.efimenko1488@gmail.com>
2023-10-12 23:24:52 +0300

    added logger and postman.json

--- src/main/java/com/example/TomTomIntegration/service/PoiLogServiceImpl.java
+++ src/main/java/com/example/TomTomIntegration/service/PoiLogServiceImpl.java
@@ -4,8 +4,10 @@ import com.example.TomTomIntegration.mapper.PoiLogMapper;
 import com.example.TomTomIntegration.messaging.message.PoiLogMessage;
 import com.example.TomTomIntegration.repository.PoiLogRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class PoiLogServiceImpl implements PoiLogService {
@@ -16,6 +18,7 @@ public class PoiLogServiceImpl implements PoiLogService {
 
     @Override
     public void createPoiLog(PoiLogMessage message) {
+        log.info("Create poi log with id [{}]", message.getPoi().getId());
         poiLogRepository.save(poiLogMapper.mapToPoiLogsEntity(message));
     }
 }
